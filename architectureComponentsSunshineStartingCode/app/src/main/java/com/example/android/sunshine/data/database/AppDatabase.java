0fcf1210b11bd68735e3beb0d65d0fbf2db89ef6
HOUDAYER CORENTIN <20181166@webmail.universita.corsica>
2018-12-04 15:34:22 +0100

    Room database setup

---
+++ architectureComponentsSunshineStartingCode/app/src/main/java/com/example/android/sunshine/data/database/AppDatabase.java
@@ -0,0 +1,36 @@
+package com.example.android.sunshine.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+@Database(entities = {WeatherEntry.class}, version = 1) //Entities listed here
+@TypeConverters(DateConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "weather";
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static volatile AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, AppDatabase.DATABASE_NAME).build();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    /**
+     * Methods to get the weather DAO
+     * @return
+     */
+    public abstract WeatherDAO weatherDAO(); //Getters for Dao
+
+}
--- architectureComponentsSunshineStartingCode/app/src/main/java/com/example/android/sunshine/data/database/WeatherDAO.java
+++ architectureComponentsSunshineStartingCode/app/src/main/java/com/example/android/sunshine/data/database/WeatherDAO.java
@@ -2,11 +2,18 @@ package com.example.android.sunshine.data.database;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.Date;
 
 @Dao
 public interface WeatherDAO {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // REPLACE STRATEGY - old valued get replaced by newest ones
     void bulkInsert(WeatherEntry... weather);
+
+    @Query("SELECT * FROM weather WHERE date = :date")
+    WeatherEntry getWeatherByDate(Date date);
 
 }
